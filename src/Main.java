82562012e484525c43d5d5279145486b4a92aa35
Anna Adamik <quenthel5@gmail.com>
2023-03-10 09:47:04 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PositiveNegativeZero.checkNumber(5);
+        PositiveNegativeZero.checkNumber(-10);
+        PositiveNegativeZero.checkNumber(0);
+    }
+}
